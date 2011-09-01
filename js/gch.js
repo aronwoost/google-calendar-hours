3e82496dac8168818ed7a9e5ea95ddd958907288
Aron Woost <aron@springworks.de>
2011-09-01 17:19:43 +0200

    auth chain basic implementation

--- js/gch.js
+++ js/gch.js
@@ -1,10 +1,27 @@
 $(document).ready(function(){
 	
-	console.log("ready");
+	var authObj = sessionStorage.getItem("auth");
 	
-	auth();
+	if(authObj) {
+		var authJson = JSON.parse(authObj);
+		console.dir(authJson);
+	}
+	
+	$("#main").find("#auth").click(function(event){
+		event.preventDefault();
+		auth();
+	});
 });
 
 function auth() {
-
+	
+	var clientId = "502172359025.apps.googleusercontent.com";
+	var callbackUrl = "http://aronwoost.github.com/google-calendar-hours/auth.html";
+	var scope = "https://www.google.com/calendar/feeds/";
+	
+	var regUrl = "https://accounts.google.com/o/oauth2/auth?client_id="+clientId+"&redirect_uri="+callbackUrl+"&scope="+scope+"&response_type=token";
+	
+	console.log(regUrl);
+	
+	window.location = regUrl;
 };
