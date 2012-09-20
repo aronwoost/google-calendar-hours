d2b762fe4eecb06064011ffe17fb12fcb29d27ad
Aron Woost <aron@springworks.de>
2012-09-20 13:35:09 +0200

    RangeModel tests finished

--- test/specs/test.RangeModel.js
+++ test/specs/test.RangeModel.js
@@ -17,23 +17,41 @@ describe("RangeModel", function() {
 
 	});
 
-	it('should return week range', function (done) {
+	it('should return week range (sunday)', function (done) {
 
 		var rangeModel = new RangeModel();
 		rangeModel.updateRangeByIndex(1);
 		var rangeObj = rangeModel.getRangeObj();
 		expect(rangeObj.type).to.be("week");
-		expect(rangeObj.start.toString()).to.be(Date.today().moveToDayOfWeek(0, -1).toString());
-		//FIXME
-		//expect(rangeObj.end.toString()).to.be(Date.today().add(1).weeks().moveToDayOfWeek(0, -1).toString());
+		var d1 = Date.today().moveToDayOfWeek(0, -1);
+		expect(rangeObj.start.toString()).to.be(d1.toString());
+		expect(rangeObj.end.toString()).to.be(d1.clone().addDays(6).addHours(23).addMinutes(59).addSeconds(59).toString());
 		rangeModel.changeRange(1);
 		rangeObj = rangeModel.getRangeObj();
 		expect(rangeObj.type).to.be("week");
-		expect(rangeObj.start.toString()).to.be(Date.today().addWeeks(1).moveToDayOfWeek(0, -1).toString());
-		//FIXME
-		//expect(rangeObj.end.toString()).to.be(Date.today().add(2).days().toString());
-		//FIXME
-		//add tests for monday/sunday switch
+		d1 = Date.today().addWeeks(1).moveToDayOfWeek(0, -1);
+		expect(rangeObj.start.toString()).to.be(d1.toString());
+		expect(rangeObj.end.toString()).to.be(d1.clone().addDays(6).addHours(23).addMinutes(59).addSeconds(59).toString());
+		done();
+
+	});
+
+	it('should return week range (monday)', function (done) {
+
+		var rangeModel = new RangeModel();
+		rangeModel.updateRangeByIndex(1);
+		rangeModel.updateWeekStart("monday");
+		var rangeObj = rangeModel.getRangeObj();
+		expect(rangeObj.type).to.be("week");
+		var d1 = Date.today().clone().monday().addWeeks(-1);
+		expect(rangeObj.start.toString()).to.be(d1.toString());
+		expect(rangeObj.end.toString()).to.be(d1.clone().addDays(6).addHours(23).addMinutes(59).addSeconds(59).toString());
+		rangeModel.changeRange(1);
+		rangeObj = rangeModel.getRangeObj();
+		expect(rangeObj.type).to.be("week");
+		d1 = Date.today().clone().monday();
+		expect(rangeObj.start.toString()).to.be(d1.toString());
+		expect(rangeObj.end.toString()).to.be(d1.clone().addDays(6).addHours(23).addMinutes(59).addSeconds(59).toString());
 		done();
 
 	});
@@ -80,12 +98,6 @@ describe("RangeModel", function() {
 		expect(rangeObj.type).to.be("total");
 		expect(rangeObj.start).to.be(0);
 		expect(rangeObj.end.toString()).to.be("Infinity");
-		//FIXME why does changeRange() gives error?
-		// rangeModel.changeRange(1);
-		// rangeObj = rangeModel.getRangeObj();
-		// expect(rangeObj.type).to.be("total");
-		// expect(rangeObj.start).to.be(0);
-		// expect(rangeObj.end.toString()).to.be("Infinity");
 		done();
 
 	});
