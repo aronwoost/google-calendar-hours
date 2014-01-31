7e175103a29e2e9289a64d15425812f45a581fd0
Aron Woost <aron@springworks.de>
2014-01-31 16:52:47 +0100

    refactor range change btns, simplify and clean up

--- js/app/view/range_change_btns_view.js
+++ js/app/view/range_change_btns_view.js
@@ -15,65 +15,33 @@ define([
   };
 
   var RangeChangeBtns = Backbone.View.extend({
+    className: "range-change-btns btn-group",
     template: _.template(rangeChangeBtns),
     events: {
-      "click a#prev": "changeRangePrev",
-      "click a#reset": "changeRangeReset",
-      "click a#next": "changeRangeNext"
+      "click a": "changeRange"
     },
     initialize: function() {
       this.model.bind("change:range", this.update, this);
     },
     render: function() {
-      this.$el.css("display", "none");
-      this.$el.css("text-align", "center");
-      this.$el.css("margin-bottom", "9px");
-      this.$el.addClass("btn-group");
-      this.$el.html(this.template({
-        to: "",
-        disableBtns: null
-      }));
-      this.update(this.model, this.model.get("range"));
+      this.update();
       return this.$el;
     },
-    disableBtns: false,
-    update: function(model, value) {
-      if(model.get("range") === "custom") {
-        this.$el.hide();
-      } else {
-        this.$el.show();
-      }
+    update: function() {
+      var value = this.model.get("range");
 
-      var middleBtnLabel = btnLabel[value] || "";
-      this.disableBtns = value === "total";
       this.$el.html(this.template({
-        to: middleBtnLabel,
-        disableBtns: this.disableBtns
+        hide: this.model.get("range") === "custom",
+        to: btnLabel[value] || "",
+        disableBtns: value === "total"
       }));
     },
-    changeRangePrev: function(evt) {
+    changeRange: function(evt) {
+      var change = parseInt(this.$(evt.target).data("change"), 10);
+
       evt.preventDefault();
-      if(this.disableBtns){
-        return;
-      }
-      this.model.changeRange(-1);
-    },
-    changeRangeReset: function(evt) {
-      evt.preventDefault();
-      if(this.disableBtns){
-        return;
-      }
-      this.model.changeRange(0);
-    },
-    changeRangeNext: function(evt) {
-      evt.preventDefault();
-      if(this.disableBtns){
-        return;
-      }
-      this.model.changeRange(1);
-    },
-    show:function(){
-      this.$el.show();
+
+      this.model.changeRange(change);
     }
   });
 
