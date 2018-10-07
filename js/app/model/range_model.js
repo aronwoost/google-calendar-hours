7eb421d12b578daafa01a8c83d214df0633747d9
Philipp Kyeck <philipp@beta-interactive.de>
2018-10-07 12:56:47 +0200

    Add sorting + exporting of monthly data (#37)

--- js/app/model/range_model.js
+++ js/app/model/range_model.js
@@ -18,15 +18,19 @@ define([
       this.currentDatePointer = moment().startOf("day");
       this.currentDatePointerEnd = moment().startOf("day");
       this.weekStart = "sunday";
+      this.sortBy = "amount";
 
-      if(config) {
-        if(config.lastSelectedRangeIndex !== null) {
+      if (config) {
+        if (config.lastSelectedRangeIndex !== null) {
           this.updateRangeByIndex(config.lastSelectedRangeIndex);
-          if(config.lastSelectedRangeIndex === 5) {
+          if (config.lastSelectedRangeIndex === 5) {
             this.updateCustomRange(config.customStart, config.customEnd);
           }
-          if(config.weekStart) {
+          if (config.weekStart) {
             this.updateWeekStart(config.weekStart);
+          }
+          if (config.sortBy) {
+            this.updateSortBy(config.sortBy);
           }
         } else {
           this.updateRangeByIndex(2);
@@ -48,31 +52,39 @@ define([
       var range = this.get("range"),
         d1, d2;
 
-      if(range === "day") {
+      if (range === "day") {
         d1 = this.currentDatePointer.clone();
         d2 = this.currentDatePointer.clone().add("days", 1);
-      } else if(range === "week") {
-        if(this.weekStart === "sunday") {
+      } else if (range === "week") {
+        if (this.weekStart === "sunday") {
           d1 = this.currentDatePointer.clone().day(0);
         } else {
           d1 = this.currentDatePointer.clone().day(1);
         }
         d2 = d1.clone().add("weeks", 1);
-      } else if(range === "month") {
+      } else if (range === "month") {
         d1 = this.currentDatePointer.clone().startOf("month");
         d2 = this.currentDatePointer.clone().startOf("month").add("month", 1);
-      } else if(range === "year") {
+      } else if (range === "year") {
         d1 = this.currentDatePointer.clone().startOf("year");
         d2 = d1.clone().add("year", 1);
-      } else if(range === "total") {
+      } else if (range === "total") {
         d1 = moment(0);
         d2 = moment("Dec 31, 2040");
-      } else if(range === "custom") {
+      } else if (range === "custom") {
         d1 = this.currentDatePointer.clone();
         d2 = this.currentDatePointerEnd.clone();
       }
 
-      this.set({rangeObj:{start:d1, end:d2, type:range, weekStart:this.weekStart}});
+      this.set({
+        rangeObj: {
+          start: d1,
+          end: d2,
+          type: range,
+          weekStart: this.weekStart,
+          sortBy: this.sortBy,
+        }
+      });
     },
     getRangeObj: function() {
       return this.get("rangeObj");
@@ -80,36 +92,46 @@ define([
     changeRange: function(direction, custom) {
       var range = this.get("range");
 
-      if(direction === 0) {
+      if (direction === 0) {
         this.currentDatePointer = moment().startOf("day");
         this.updateRangeObj();
         return;
       }
 
-      if(range === "day") {
+      if (range === "day") {
         this.currentDatePointer.add("days", direction);
-      } else if(range === "week") {
+      } else if (range === "week") {
         this.currentDatePointer.add("weeks", direction);
-      } else if(range === "month") {
+      } else if (range === "month") {
         this.currentDatePointer.add("months", direction);
-      } else if(range === "year") {
+      } else if (range === "year") {
         this.currentDatePointer.add("years", direction);
-      } else if(range === "custom") {
+      } else if (range === "custom") {
         this.currentDatePointer = custom.start;
         this.currentDatePointerEnd = custom.end;
       }
       this.updateRangeObj();
     },
     updateWeekStart: function(day) {
-      if(this.weekStart === day) {
+      if (this.weekStart === day) {
         return;
       }
       this.weekStart = day;
       this.updateRangeObj();
     },
-    getWeekStart: function(){
+    getWeekStart: function() {
       return this.weekStart;
-    }
+    },
+    updateSortBy: function(sort) {
+      if (this.sortBy === sort) {
+        return;
+      }
+      this.sortBy = sort;
+      this.updateRangeObj();
+    },
+    getSortBy: function() {
+      return this.sortBy;
+    },
   });
 
   return RangeModel;
--- js/app/view/output_view.js
+++ js/app/view/output_view.js
@@ -3,10 +3,11 @@ define([
     "backbone",
     "underscore",
     "text!app/templates/output.html",
+    "moment",
     "bootstrap-collapse",
     "bootstrap-transition"
   ],
-  function ($, Backbone, _, outputTmpl) {
+  function ($, Backbone, _, outputTmpl, moment) {
 
   "use strict";
 
@@ -36,24 +37,40 @@ define([
       var calendar = this.model.selectedCalendar;
 
       // waiting for user to select calendar
-      if(!calendar) {
+      if (!calendar) {
         return;
       }
 
-      if(!calendar.isSynced()) {
+      if (!calendar.isSynced()) {
         var spinnerContainer = $("<div id='spinnerContainer' style='position:relative; left:150px; top:40px;'></div>");
         var spinner = spinnerContainer.spin(spinnerOptions);
         this.$el.html(spinnerContainer);
         return;
       }
 
-      var data = calendar.getHours(this.model.selectedRange.getRangeObj());
+      var rangeObj = this.model.selectedRange.getRangeObj();
+      var data = calendar.getHours(rangeObj);
+      var hours = Math.round(data.total*100)/100;
+      var range = "";
+      var $showDetails;
 
-      var hours = Math.round(data.total*100)/100,
-        rangeObj = this.model.getSelectedRange(),
-        range = "",
-        $showDetails;
+      var downloadLink = "";
+      var filename = "";
 
+      if (rangeObj.sortBy === "date") {
+        downloadLink = "Start,End,Title,Hours\n";
+        
+        for (var i = 0; i < data.projects.length; i++) {
+          downloadLink += moment(data.projects[i].date).format("DD.MM.YYYY HH:mm") + ",";
+          downloadLink += moment(data.projects[i].end).format("DD.MM.YYYY HH:mm") + ",";
+          downloadLink += "\"" + data.projects[i].label + "\",";
+          downloadLink += data.projects[i].hours;
+          downloadLink += "\n";
+        }
+
+        downloadLink = this._getBlobUrl(downloadLink);
+        filename = calendar.get("summary").replace("\"","") + "_" + rangeObj.start.format("MMMM_YYYY") + "_(" + moment().format("YYYYMMDDHHmmss") + ").csv";
+      }
 
       if (rangeObj.type === "day") {
         range = rangeObj.start.format("dddd, MMMM D, YYYY");
@@ -68,7 +85,11 @@ define([
       this.$el.html(this.template({
         hours: hours,
         projects: data.projects,
-        range: range
+        range: range,
+        sortBy: rangeObj.sortBy,
+        outputDate: this.outputDate,
+        downloadLink: downloadLink,
+        filename: filename
       }));
 
       // add listener onto details collapse thingy to save state
@@ -82,11 +103,20 @@ define([
     show: function() {
       this.$el.show();
     },
+    outputDate: function(date) {
+      return moment(date).format("DD.MM.");
+    },
     onDetailsShown: function() {
       this.detailsShown = true;
     },
     onDetailsHidden: function() {
       this.detailsShown = false;
+    },
+    _getBlobUrl: function(content) {
+      var MIME_TYPE = "text/csv;charset=UTF-8";
+      var UTF8_BOM = "\uFEFF";
+      var blob = new Blob([UTF8_BOM + content], {type:MIME_TYPE});
+      return window.URL.createObjectURL(blob);
     }
   });
 
