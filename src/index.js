3383b4e2930fcf762a2db9dccb6fa14e21bd4815
Aron Woost <aron@springworks.de>
2020-10-04 16:51:07 +0200

    commit eslint error

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
-import App from './App';
+import App from './AppX';
 import store from './stores/store';
 
 ReactDOM.render(
