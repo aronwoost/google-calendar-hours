34fbd1e3bcd9e76e6018e62ea68156c9fa6c5b78
Aron Woost <aron@springworks.de>
2020-10-04 16:59:10 +0200

    Add prettier „error“

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
-import App from './App';
+import App from "./App";
 import store from './stores/store';
 
 ReactDOM.render(
