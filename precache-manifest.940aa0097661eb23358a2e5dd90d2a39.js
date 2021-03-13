1ea74987e846f1996b3a057f0d6bfde21213199e
aronwoost <aron@springworks.de>
2021-03-13 15:23:14 +0000

    Update test-deploy to output generated at 3ec025f

---
+++ precache-manifest.940aa0097661eb23358a2e5dd90d2a39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d75b8028cefa61d0c55b0c62e22a9a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db00d131628a5b3638c0",
+    "url": "/static/css/2.3331bcfc.chunk.css"
+  },
+  {
+    "revision": "e291f88305fe5824e1c0",
+    "url": "/static/css/main.3080f13a.chunk.css"
+  },
+  {
+    "revision": "db00d131628a5b3638c0",
+    "url": "/static/js/2.d0f07bfc.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.d0f07bfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e291f88305fe5824e1c0",
+    "url": "/static/js/main.2e2f087a.chunk.js"
+  },
+  {
+    "revision": "4a42fdcbdfd3cf55fea3",
+    "url": "/static/js/runtime-main.1c47a842.js"
+  }
+]);
