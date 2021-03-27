0109a234c8ac84af593ce8f361b9492d00e8fee3
Aron Woost <aron@springworks.de>
2021-03-27 17:55:56 +0100

    Use async/await in api.js

--- src/stores/api.js
+++ src/stores/api.js
@@ -3,26 +3,27 @@ import dayjs from 'dayjs';
 
 const API_BASE_PATH = 'https://www.googleapis.com/calendar/v3/';
 
-const fetchGoogle = ({ url, accessToken, params }) =>
-  fetch(
+const fetchGoogle = async ({ url, accessToken, params }) => {
+  const response = await fetch(
     `${url}?${queryString.stringify({
       access_token: accessToken,
       ...params,
     })}`
-  ).then((response) => {
-    if (response.status !== 200) {
-      sessionStorage.removeItem('accessToken');
-      window.location = '/';
-      return Promise.reject();
-    }
-    return response.json();
-  });
+  );
+
+  if (response.status !== 200) {
+    sessionStorage.removeItem('accessToken');
+    window.location = '/';
+    return Promise.reject();
+  }
+  return response.json();
+};
 
 export const fetchCalendars = ({ accessToken }) =>
   fetchGoogle({ url: `${API_BASE_PATH}users/me/calendarList`, accessToken });
 
-const fetchEvents = ({ accessToken, calendarId, pageToken, acc }) =>
-  fetchGoogle({
+const fetchEvents = async ({ accessToken, calendarId, pageToken, acc }) => {
+  const { items, nextPageToken } = await fetchGoogle({
     url: `${API_BASE_PATH}calendars/${calendarId}/events`,
     accessToken,
     params: {
@@ -31,18 +32,19 @@ const fetchEvents = ({ accessToken, calendarId, pageToken, acc }) =>
       pageToken,
       timeMax: dayjs().add(1, 'year').toJSON(),
     },
-  }).then(({ items, nextPageToken }) => {
-    if (!nextPageToken) {
-      return [...acc, ...items];
-    }
-
-    return fetchEvents({
-      accessToken,
-      calendarId,
-      pageToken: nextPageToken,
-      acc: [...acc, ...items],
-    });
   });
+
+  if (!nextPageToken) {
+    return [...acc, ...items];
+  }
+
+  return fetchEvents({
+    accessToken,
+    calendarId,
+    pageToken: nextPageToken,
+    acc: [...acc, ...items],
+  });
+};
 
 export const fetchCalendarEvents = async ({ accessToken, calendarId }) =>
   fetchEvents({ accessToken, calendarId, acc: [] });
