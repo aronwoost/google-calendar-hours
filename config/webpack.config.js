ae19b50bbcbef42397ede034f6f8706edaa1244c
Aron Woost <aron@springworks.de>
2021-04-05 14:16:32 +0200

    Setup prerender

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -514,7 +514,7 @@ module.exports = function(webpackEnv) {
           {},
           {
             inject: true,
-            template: paths.appHtml,
+            template: `!!prerender-loader?string!${paths.appHtml}`,
           },
           isEnvProduction
             ? {
