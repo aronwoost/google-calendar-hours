78d7fd21611775a5ef5bb5e75787220614e31f07
Aron Woost <aron@springworks.de>
2021-04-08 14:03:05 +0200

    Install RelativeCI

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -27,6 +27,7 @@ const getClientEnvironment = require('./env');
 const ModuleNotFoundPlugin = require('react-dev-utils/ModuleNotFoundPlugin');
 const ForkTsCheckerWebpackPlugin = require('react-dev-utils/ForkTsCheckerWebpackPlugin');
 const typescriptFormatter = require('react-dev-utils/typescriptFormatter');
+const { RelativeCiAgentWebpackPlugin } = require('@relative-ci/agent');
 
 const postcssNormalize = require('postcss-normalize');
 
@@ -664,6 +665,7 @@ module.exports = function(webpackEnv) {
           // The formatter is invoked directly in WebpackDevServerUtils during development
           formatter: isEnvProduction ? typescriptFormatter : undefined,
         }),
+      new RelativeCiAgentWebpackPlugin(),
     ].filter(Boolean),
     // Some libraries import Node modules but don't use them in the browser.
     // Tell webpack to provide empty mocks for them so importing them works.
