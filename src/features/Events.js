3137caea700e6db8f055bd0466f87ae04a2ac100
Aron Woost <aron@springworks.de>
2023-06-18 15:43:22 +0200

    Implement

--- src/features/Events.js
+++ src/features/Events.js
@@ -44,6 +44,14 @@ const sortByStart = ({ start: startA }, { start: startB }) => {
   return 0;
 };
 
+const copyToClipboard = async (text) => {
+  try {
+    await navigator.clipboard.writeText(text);
+  } catch (err) {
+    // don't handle
+  }
+};
+
 const format = (date) => dayjs(date).format(EXPORT_DATE_FORMAT);
 
 const Events = () => {
@@ -138,6 +146,26 @@ const Events = () => {
               >
                 {summary}
               </span>
+              <button
+                type="button"
+                className={styles.copyButton}
+                onClick={() => copyToClipboard(summary)}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <rect x="9" y="9" width="13" height="13" rx="2" ry="2" />
+                  <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1" />
+                </svg>
+              </button>
               <span
                 className={cx(bootstrap['col-sm'], styles.eventHours)}
               >{`${roundHours(hours)}h`}</span>
