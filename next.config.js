b4f33d939440182f337bb28a61b668674f4628ed
Aron Woost <aron@springworks.de>
2024-01-12 22:35:44 +0100

    Bring back CI stats

--- next.config.js
+++ next.config.js
@@ -1,9 +1,30 @@
+const { StatsWriterPlugin } = require('webpack-stats-plugin')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack: (config, options) => {
+    const { dev, isServer } = options;
+
+    // Output webpack stats JSON file only for client-side/production build
+    if (!dev && !isServer) {
+      config.plugins.push(
+        new StatsWriterPlugin({
+          filename: '../webpack-stats.json',
+          stats: {
+            assets: true,
+            chunks: true,
+            modules: true
+          }
+        })
+      );
+    }
+
+    return config;
+  }
 };
 
 module.exports = nextConfig;
