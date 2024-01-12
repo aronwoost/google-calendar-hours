714aa08ba32b54489e5fe6e40f1cd64fcbea6db8
Aron Woost <aron@springworks.de>
2024-01-12 14:29:50 +0100

    Delete2

--- src2/utils/formatDate.js
+++
@@ -1,4 +0,0 @@
-const formatDate = (date, options) =>
-  new Intl.DateTimeFormat([navigator.language, 'en-US'], options).format(date);
-
-export default formatDate;
--- src2/utils/roundHours.js
+++
@@ -1,3 +0,0 @@
-const roundHours = (number) => Math.round(number * 100) / 100;
-
-export default roundHours;
