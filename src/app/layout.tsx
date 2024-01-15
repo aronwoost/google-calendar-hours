b16cdb382f7cccc7877b9aa0e2de38d036235594
Aron Woost <aron@springworks.de>
2024-01-15 11:41:33 +0100

    Add viewport meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,11 @@ export const metadata: Metadata = {
   description: '',
 };
 
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+};
+
 export default function RootLayout({
   children,
 }: {
